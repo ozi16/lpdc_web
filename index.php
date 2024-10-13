832d8382b8d3ac3249a1f180f307f180699cf72d
fahrurozi16 <muhammadfahrurozi518@gmail.com>
2024-10-14 00:09:03 +0700

    first Commit

---
+++ index.php
@@ -0,0 +1,292 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="./assets/css/style.css" />
+
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <!-- bootstrap CSS -->
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css" />
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="container border">
+        <nav class="navbar navbar-expand-lg bg-transparent">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"
+              ><img src="./assets/img/logo.png" alt="logo"
+            /></a>
+            <div class="navbar-header">
+              <!-- Toggle button -->
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+            </div>
+            <div
+              class="collapse navbar-collapse justify-content-center"
+              id="navbarNav"
+            >
+              <ul class="navbar-nav align-items-center text-center border">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#"
+                    >Retail</a
+                  >
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">SME</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">CORPORATE</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">COVID 19</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">CAMPGAINS</a>
+                </li>
+              </ul>
+              <div
+                class="pbmit-right-box d-flex align-items-center justify-content-center me-2 border"
+              >
+                <a href="#" class="pe-3"><i data-feather="search"></i></a>
+                <button class="btn btn-primary">ISERVICE</button>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <!-- Start Hero -->
+      <section class="border">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg12 col-xl-6 col-12 border">
+              <div class="border mt-lg-5 p-2">
+                <h1 class="fs-1 fw-bold">Chase your</h1>
+                <h1 class="fs-1 fw-bold">dream with us</h1>
+                <p class="ep-hero_text text-secondary w-75 border">
+                  The harder you work for something, the greater you’ll feel
+                  when you achieve it.
+                </p>
+              </div>
+              <div class="d-flex border">
+                <div class="align-items-center p-2">
+                  <button class="btn btn-primary me-3">Apply online</button>
+                  <button class="btn btn-outline-primary">
+                    Loan Calculator
+                  </button>
+                </div>
+              </div>
+              <div class="d-flex mt-5">
+                <div class=""><i data-feather="phone"></i> 16519</div>
+              </div>
+            </div>
+            <div class="col-lg-12 col-xl-6 col-12 order-top">
+              <div class="ep-hero__widget border">
+                <!-- Widget one -->
+                <div class="ep-hero__widget">
+                  <div class="ep-hero__widget-column">
+                    <!-- btn 1 -->
+                    <div class="ep-hero__widget-btn1 btn btn-primary bg-white">
+                      <a href="#" class="none">
+                        <i data-feather="shopping-cart"></i>Car Loans
+                      </a>
+                    </div>
+                    <!-- image1 -->
+                    <div class="ep-hero__widget-img container">
+                      <img
+                        class="img-thumbnail"
+                        src="./assets/img/family.png"
+                        alt="family"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End Hero -->
+
+      <!-- Start About -->
+      <section class="border pt-2 border">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-8 col-xl-4 col-md-8 col-12 border w-100">
+              <div
+                class="text-center justify-content-center align-content-center border"
+              >
+                <h1 class="fw-bold fs-3 border">
+                  Creating Extraordinary Customer Experience
+                </h1>
+                <div class="w-75 justify-content-center">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dignissimos obcaecati ullam perferendis magni aspernatur
+                    maiores ratione repellat deserunt quas aut cum cupiditate
+                    adipisci iure, commodi dolorem veritatis cumque, harum
+                    velit!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <!-- single card -->
+            <div class="col-lg-4 col-xl-3 col-md-6 col-12">
+              <div class="card mt-3">
+                <div class="bg-transparent">
+                  <i data-feather="briefcase"></i>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Integrity</h5>
+                  <p>
+                    Displaying the highest level of Integrity in the way we
+                    conduct our business
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- single card -->
+            <div class="col-lg-4 col-xl-3 col-md-6 col-12">
+              <div class="card mt-3">
+                <div class="bg-transparent">
+                  <i data-feather="briefcase"></i>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Integrity</h5>
+                  <p>
+                    Displaying the highest level of Integrity in the way we
+                    conduct our business
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- single card -->
+            <div class="col-lg-4 col-xl-3 col-md-6 col-12">
+              <div class="card mt-3">
+                <div class="bg-transparent">
+                  <i data-feather="briefcase"></i>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Integrity</h5>
+                  <p>
+                    Displaying the highest level of Integrity in the way we
+                    conduct our business
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- single card -->
+            <div class="col-lg-4 col-xl-3 col-md-6 col-12">
+              <div class="card mt-3">
+                <div class="bg-transparent">
+                  <i data-feather="briefcase"></i>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Integrity</h5>
+                  <p>
+                    Displaying the highest level of Integrity in the way we
+                    conduct our business
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- single card -->
+            <div class="col-lg-4 col-xl-3 col-md-6 col-12">
+              <div class="card mt-3">
+                <div class="bg-transparent">
+                  <i data-feather="briefcase"></i>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Integrity</h5>
+                  <p>
+                    Displaying the highest level of Integrity in the way we
+                    conduct our business
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- single card -->
+            <div class="col-lg-4 col-xl-3 col-md-6 col-12">
+              <div class="card mt-3">
+                <div class="bg-transparent">
+                  <i data-feather="briefcase"></i>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Integrity</h5>
+                  <p>
+                    Displaying the highest level of Integrity in the way we
+                    conduct our business
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- single card -->
+            <div class="col-lg-4 col-xl-3 col-md-6 col-12">
+              <div class="mt-3">
+                <div class="bg-transparent">
+                  <i data-feather="briefcase"></i>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Integrity</h5>
+                  <p>
+                    Displaying the highest level of Integrity in the way we
+                    conduct our business
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- single card -->
+            <div class="col-lg-4 col-xl-3 col-md-6 col-12">
+              <div class="card mt-3">
+                <div class="bg-transparent">
+                  <i data-feather="briefcase"></i>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Integrity</h5>
+                  <p>
+                    Displaying the highest level of Integrity in the way we
+                    conduct our business
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Business Partners -->
+    </div>
+
+    <script>
+      feather.replace();
+    </script>
+    <script src="./assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
